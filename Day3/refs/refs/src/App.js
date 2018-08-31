dabc0ba71f926c582ea96811a55ac976575acbaa
Narmadha6790 <narmadha.raju@gmail.com>
2018-08-31 09:48:01 +0530

    Add files via upload

---
+++ Day3/refs/refs/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+// import PostSorting from './posts/PostSorting'
+import "./App.css";
+
+import ImgDropAndCrop from "./learn/ImgDropAndCrop";
+import FormsAndInputs from "./learn/FormsAndInputs";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FormsAndInputs />
+      </div>
+    );
+  }
+}
+
+export default App;
